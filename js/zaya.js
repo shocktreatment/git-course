1543383ec809c0ed5afffdadfde224e86d286fdb
shock_treatment <shock_treatment@icloud.com>
2023-01-07 22:18:43 +0200

    create minNumber

--- js/zaya.js
+++ js/zaya.js
@@ -1,1 +1,10 @@
 console.log('ZAYA');
+
+const numbers = [3, 5, 6, 7, 2, 1];
+let min = numbers[0];
+for (const number of numbers) {
+    if (number < min) {
+        min = number;
+    }
+}
+console.log(min);
